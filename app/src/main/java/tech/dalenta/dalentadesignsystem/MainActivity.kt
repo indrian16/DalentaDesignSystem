a303d3212c5db47d62fa0202a5c7a266a844cae0
INDRIAN <indrian16@outlook.co.id>
2021-04-06 12:44:38 +0800

    add Large button: Primary, Secondary, Outlined

--- app/src/main/java/tech/dalenta/dalentadesignsystem/MainActivity.kt
+++ app/src/main/java/tech/dalenta/dalentadesignsystem/MainActivity.kt
@@ -3,9 +3,7 @@ package tech.dalenta.dalentadesignsystem
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import tech.dalenta.component.SmallButtonOutlined
-import tech.dalenta.component.SmallButtonPrimary
-import tech.dalenta.component.SmallButtonSecondary
+import tech.dalenta.component.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +21,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
         findViewById<SmallButtonOutlined>(R.id.btn_small_outlined).apply {
-            button.text = "SmallButtonOutlined Edited"
+            //button.text = "SmallButtonOutlined Edited"
             setOnClickListener {
                 Toast.makeText(it.context, "SmallButtonOutlined", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        findViewById<LargeButtonPrimary>(R.id.btn_large_primary).apply {
+            setOnClickListener {
+                Toast.makeText(it.context, "LargeButtonPrimary", Toast.LENGTH_SHORT).show()
+            }
+        }
+        findViewById<LargeButtonSecondary>(R.id.btn_large_secondary).apply {
+            setOnClickListener {
+                Toast.makeText(it.context, "LargeButtonSecondary", Toast.LENGTH_SHORT).show()
+            }
+        }
+        findViewById<LargeButtonOutlined>(R.id.btn_large_outlined).apply {
+            setOnClickListener {
+                Toast.makeText(it.context, "LargeButtonOutlined", Toast.LENGTH_SHORT).show()
             }
         }
     }
---
+++ component/src/main/java/tech/dalenta/component/LargeButtonPrimary.kt
@@ -0,0 +1,47 @@
+package tech.dalenta.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.widget.FrameLayout
+import com.google.android.material.button.MaterialButton
+
+class LargeButtonPrimary(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+    var button: MaterialButton
+    var parentView: FrameLayout
+
+    init {
+        inflate(context, R.layout.large_button_primary, this)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.LargeButtonPrimary)
+
+        val dimensionWidth = attributes.getLayoutDimension(R.styleable.Button_android_layout_width, LayoutParams.WRAP_CONTENT)
+        val dimensionHeight = attributes.getLayoutDimension(R.styleable.Button_android_layout_height, LayoutParams.WRAP_CONTENT)
+
+        val params = LayoutParams(
+                dimensionWidth,
+                dimensionHeight
+        )
+
+        button = findViewById(R.id.button)
+        button.apply {
+            text = attributes.getString(R.styleable.Button_android_text) ?: "Button"
+            layoutParams = params
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, attributes.getDimension(R.styleable.Button_android_textSize, this.textSize))
+        }
+
+        parentView = findViewById(R.id.parent_view)
+        if (dimensionWidth == LayoutParams.MATCH_PARENT) {
+            parentView.layoutParams.width = LayoutParams.MATCH_PARENT
+        }
+        if (dimensionHeight == LayoutParams.MATCH_PARENT) {
+            parentView.layoutParams.height = LayoutParams.MATCH_PARENT
+        }
+
+        attributes.recycle()
+    }
+
+    override fun setOnClickListener(l: OnClickListener?) {
+        button.setOnClickListener(l)
+    }
+}
