d2511808f28bc13f0c39187cebad3a15f1107e92
INDRIAN <indrian16@outlook.co.id>
2021-04-15 22:13:07 +0800

    improve getOverflowDimension

--- component/src/main/java/tech/dalenta/component/utils/ViewUtils.kt
+++ component/src/main/java/tech/dalenta/component/utils/ViewUtils.kt
@@ -3,12 +3,6 @@ package tech.dalenta.component.utils
 
 object ViewUtils {
 
-    fun getOverflowDimension(dimension: Int): Int {
-        return if (dimension == -1 || dimension == 0) {
-            -1
-        } else {
-            dimension
-        }
-    }
+    fun getOverflowDimension(dimension: Int) = if (dimension == 0) -1 else dimension
 
 }
