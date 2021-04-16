18ff119fbd026a318cf17f245c5073eb575bc616
INDRIAN <indrian16@outlook.co.id>
2021-04-16 15:04:09 +0800

    improve large button

--- component/src/main/java/tech/dalenta/component/LargeButtonPrimary.kt
+++ component/src/main/java/tech/dalenta/component/LargeButtonPrimary.kt
@@ -5,53 +5,64 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.widget.FrameLayout
 import com.google.android.material.button.MaterialButton
+import tech.dalenta.component.utils.ViewUtils
 
 class LargeButtonPrimary(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
     var button: MaterialButton
     var parentView: FrameLayout
 
     init {
+
+        // Inflating Layout
         inflate(context, R.layout.large_button_primary, this)
 
+        // Get Attr
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.LargeButtonPrimary)
 
-        val dimensionWidth = attributes.getLayoutDimension(R.styleable.Button_android_layout_width, LayoutParams.WRAP_CONTENT)
-        val dimensionHeight = attributes.getLayoutDimension(R.styleable.Button_android_layout_height, LayoutParams.WRAP_CONTENT)
-
+        // Get Dimension
+        val dimensionWidth = ViewUtils.getOverflowDimension(attributes.getLayoutDimension(R.styleable.LargeButtonPrimary_android_layout_width, LayoutParams.WRAP_CONTENT))
+        val dimensionHeight = ViewUtils.getOverflowDimension(attributes.getLayoutDimension(R.styleable.LargeButtonPrimary_android_layout_height, LayoutParams.WRAP_CONTENT))
         val params = LayoutParams(
                 dimensionWidth,
                 dimensionHeight
         )
 
+        // Set Attr Child View
         button = findViewById(R.id.button)
         button.apply {
-            text = attributes.getString(R.styleable.Button_android_text) ?: "Button"
+
+            // Set Text attribute
+            text = attributes.getString(R.styleable.LargeButtonPrimary_android_text) ?: "Button"
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, attributes.getDimension(R.styleable.LargeButtonPrimary_android_textSize, this.textSize))
+
+            // Set Layout Size
             layoutParams = params
-            setTextSize(TypedValue.COMPLEX_UNIT_PX, attributes.getDimension(R.styleable.Button_android_textSize, this.textSize))
+
+            // Set Icon attribute
+            icon = attributes.getDrawable(R.styleable.LargeButtonPrimary_icon)
+            iconGravity = attributes.getInt(R.styleable.LargeButtonPrimary_iconGravity, button.iconGravity)
+
+            // Set Color State
+            when (attributes.getInt(R.styleable.LargeButtonPrimary_color_state, ColorState.PRIMARY)) {
+                ColorState.PRIMARY -> {
+                    changeBackgroundColor(R.color.blue_3)
+                }
+                ColorState.GRAY -> {
+                    changeBackgroundColor(R.color.grey_3)
+                }
+                ColorState.GREEN -> {
+                    changeBackgroundColor(R.color.green_3)
+                }
+                ColorState.RED -> {
+                    changeBackgroundColor(R.color.red_3)
+                }
+            }
         }
 
         parentView = findViewById(R.id.parent_view)
-        if (dimensionWidth == LayoutParams.MATCH_PARENT) {
-            parentView.layoutParams.width = LayoutParams.MATCH_PARENT
-        }
-        if (dimensionHeight == LayoutParams.MATCH_PARENT) {
-            parentView.layoutParams.height = LayoutParams.MATCH_PARENT
-        }
-
-        // Set Color State
-        when (attributes.getInt(R.styleable.LargeButtonPrimary_color_state, ColorState.PRIMARY)) {
-            ColorState.PRIMARY -> {
-                button.changeBackgroundColor(R.color.blue_3)
-            }
-            ColorState.GRAY -> {
-                button.changeBackgroundColor(R.color.grey_3)
-            }
-            ColorState.GREEN -> {
-                button.changeBackgroundColor(R.color.green_3)
-            }
-            ColorState.RED -> {
-                button.changeBackgroundColor(R.color.red_3)
-            }
+        parentView.apply {
+            layoutParams.width = dimensionWidth
+            layoutParams.height = dimensionHeight
         }
 
         attributes.recycle()
--- component/src/main/java/tech/dalenta/component/LargeButtonSecondary.kt
+++ component/src/main/java/tech/dalenta/component/LargeButtonSecondary.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.widget.FrameLayout
 import com.google.android.material.button.MaterialButton
+import tech.dalenta.component.utils.ViewUtils
 
 class LargeButtonSecondary(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
@@ -12,67 +13,70 @@ class LargeButtonSecondary(context: Context, attrs: AttributeSet?) : FrameLayout
     var parentView: FrameLayout
 
     init {
+
+        // Inflating Layout
         inflate(context, R.layout.large_button_secondary, this)
 
+        // Get Attr
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.LargeButtonSecondary)
 
-        val dimensionWidth = attributes.getLayoutDimension(R.styleable.Button_android_layout_width, LayoutParams.WRAP_CONTENT)
-        val dimensionHeight = attributes.getLayoutDimension(R.styleable.Button_android_layout_height, LayoutParams.WRAP_CONTENT)
-
+        // Get Dimension
+        val dimensionWidth = ViewUtils.getOverflowDimension(attributes.getLayoutDimension(R.styleable.LargeButtonSecondary_android_layout_width, LayoutParams.WRAP_CONTENT))
+        val dimensionHeight = ViewUtils.getOverflowDimension(attributes.getLayoutDimension(R.styleable.LargeButtonSecondary_android_layout_height, LayoutParams.WRAP_CONTENT))
         val params = LayoutParams(
                 dimensionWidth,
                 dimensionHeight
         )
 
+        // Set Attr Child View
         button = findViewById(R.id.button)
         button.apply {
-            text = attributes.getString(R.styleable.Button_android_text) ?: "Button"
+
+            // Set Text attribute
+            text = attributes.getString(R.styleable.LargeButtonSecondary_android_text) ?: "Button"
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, attributes.getDimension(R.styleable.LargeButtonSecondary_android_textSize, this.textSize))
+
+            // Set Layout Size
             layoutParams = params
-            setTextSize(TypedValue.COMPLEX_UNIT_PX, attributes.getDimension(R.styleable.Button_android_textSize, this.textSize))
-        }
 
-        parentView = findViewById(R.id.parent_view)
-        if (dimensionWidth == LayoutParams.MATCH_PARENT) {
-            parentView.layoutParams.width = LayoutParams.MATCH_PARENT
-        }
-        if (dimensionHeight == LayoutParams.MATCH_PARENT) {
-            parentView.layoutParams.height = LayoutParams.MATCH_PARENT
-        }
+            // Set Icon attribute
+            icon = attributes.getDrawable(R.styleable.LargeButtonSecondary_icon)
+            iconGravity = attributes.getInt(R.styleable.LargeButtonSecondary_iconGravity, button.iconGravity)
 
-        // Set Color State
-        when (attributes.getInt(R.styleable.LargeButtonSecondary_color_state, ColorState.PRIMARY)) {
-            ColorState.PRIMARY -> {
-                button.apply {
+            // Set Color State
+            when (attributes.getInt(R.styleable.LargeButtonSecondary_color_state, ColorState.PRIMARY)) {
+                ColorState.PRIMARY -> {
                     changeBackgroundColor(R.color.blue_5)
                     changeRippleColor(R.color.blue_4)
                     changeTextColor(R.color.blue_3)
                     changeIconColor(R.color.blue_3)
                 }
-            }
-            ColorState.GRAY -> {
-                button.apply {
+                ColorState.GRAY -> {
                     changeBackgroundColor(R.color.grey_5)
                     changeRippleColor(R.color.grey_4)
                     changeTextColor(R.color.grey_3)
                     changeIconColor(R.color.grey_3)
                 }
-            }
-            ColorState.GREEN -> {
-                button.apply {
+                ColorState.GREEN -> {
                     changeBackgroundColor(R.color.green_5)
                     changeRippleColor(R.color.green_4)
                     changeTextColor(R.color.green_3)
                     changeIconColor(R.color.green_3)
                 }
-            }
-            ColorState.RED -> {
-                button.apply {
+                ColorState.RED -> {
                     changeBackgroundColor(R.color.red_5)
                     changeRippleColor(R.color.red_4)
                     changeTextColor(R.color.red_3)
                     changeIconColor(R.color.red_3)
                 }
             }
+        }
+
+        // Set Dimension Parent View
+        parentView = findViewById(R.id.parent_view)
+        parentView.apply {
+            layoutParams.width = LayoutParams.MATCH_PARENT
+            layoutParams.height = LayoutParams.MATCH_PARENT
         }
 
         attributes.recycle()
