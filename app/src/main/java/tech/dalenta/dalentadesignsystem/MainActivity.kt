633987c414464e13cc5bdd1df806f5325b4fa57c
INDRIAN <indrian16@outlook.co.id>
2021-04-22 16:56:35 +0800

    add sample login case

--- app/src/main/java/tech/dalenta/dalentadesignsystem/MainActivity.kt
+++ app/src/main/java/tech/dalenta/dalentadesignsystem/MainActivity.kt
@@ -3,6 +3,7 @@ package tech.dalenta.dalentadesignsystem
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import tech.dalenta.component.TextInput
 import tech.dalenta.component.utils.CaptionState
@@ -13,28 +14,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textInputEmail = findViewById<TextInput>(R.id.text_input_email)
-
-
         textInputEmail.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
-                if (s.isNullOrEmpty()) {
+                if (!Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()) {
                     textInputEmail.setCaptionState(CaptionState.ERROR)
-                    textInputEmail.textCaption = "Email tidak boleh kosong"
+                    textInputEmail.textCaption = "Tulis Email yang benar"
                 } else {
                     textInputEmail.setCaptionState(CaptionState.SUCCESS)
-                    textInputEmail.textCaption = "Mantap"
+                    textInputEmail.textCaption = "Goodjob"
                 }
             }
         })
 
         val textInputPassword = findViewById<TextInput>(R.id.text_input_password)
+        textInputPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if ((s?.length ?: 0) <= 6) {
+                    textInputPassword.setCaptionState(CaptionState.ERROR)
+                    textInputPassword.textCaption = "Minimal password 6 ya bro"
+                } else {
+                    textInputPassword.setCaptionState(CaptionState.SUCCESS)
+                    textInputPassword.textCaption = "Keren"
+                }
+            }
+        })
     }
 }
